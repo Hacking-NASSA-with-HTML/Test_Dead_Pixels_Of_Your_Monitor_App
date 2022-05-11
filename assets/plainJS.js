bb76e9f1ec927f32a272e18c2716385e1bbe014f
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-05-11 13:10:58 +1100

    feat: add text disappearing effect

--- assets/plainJS.js
+++ assets/plainJS.js
@@ -1,5 +1,5 @@
 
-let colorsSet = ['#009c61', '#cc0099', '#cc9900', '#cc0033', '#0099cc', '#6600cc', '#66cc00', '#f9f60d', '#14087a', 'black', 'white', '#f60808', '#2abf0b'];
+let colorsSet = ['green', '#cc0099', '#cc9900', '#cc0033', '#0099cc', '#6600cc', '#66cc00', '#f9f60d', '#14087a', 'black', 'white', '#f60808', '#2abf0b'];
 let index = 0;
 
 
@@ -18,3 +18,48 @@ document.getElementById('invisible').addEventListener('click', () => {
     index++;
 
 });
+
+
+let removeHeadline = () => {
+    let fadeTarget = document.getElementById('headline');
+    let fadeEffect = setInterval(() => {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.025;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 50); 
+}
+
+let removeManualForUser = () => {
+    let toFadeTarget = document.getElementById('manualForUser');
+    let fadeEffectForManualForUser = setInterval(() => {
+        if (!toFadeTarget.style.opacity) {
+            toFadeTarget.style.opacity = 1;
+        }
+        if (toFadeTarget.style.opacity > 0) {
+            toFadeTarget.style.opacity -= 0.025;
+        } else {
+            clearInterval(fadeEffectForManualForUser);
+        }
+    }, 50); 
+}
+
+
+
+let hideText = () => {
+    
+    setTimeout(() => {
+        removeHeadline();   
+    }, 7000);
+
+    setTimeout(() => {
+        removeManualForUser();   
+    }, 10000);
+
+}
+
+hideText();
